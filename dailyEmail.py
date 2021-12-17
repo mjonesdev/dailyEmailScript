966f305571fe4542d3737efa01d8a81377c13f2e
Matthew Jones <matt@Matthews-MacBook-Air.local>
2021-12-17 16:57:10 +0000

    keyring functionality addition and extra comments

--- dailyEmail.py
+++ dailyEmail.py
@@ -5,17 +5,23 @@ import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+import keyring
+# The password will need to have been added into the credentials manager of the system running the script before the keyring.get_password will work. This is done by, in a python terminal, typing "import keyring" followed by "keyring.set_password("nameOfStoredPassword", "usernameOfStoredPassword", "password")". You can check that this has worked by entering "keyring.get_password("nameOfStoredPassword", "usernameOfStoredPassword")" which should return the password provided. 
 
 # Variables 
 
-username = "auto@belfieldhomeandleisure.co.uk"
-password = "I1f7UEmx2B*KEx&cjY0YBQx#A@xQrT"
-mail_from = "auto@belfieldhomeandleisure.co.uk"
-mail_to = "matt.jones@stability-it.com"
-mail_subject = "Daily Documents"
-mail_body = "Auto email for the delivery of documents only. Please do not reply to this address."
+username = "email@domain.com"
+password = "app password"
+mail_from = "email@domain.com"
+mail_to = "addressee@domain.com"
+mail_subject = "Email Subject"
+# Triple quotes for multiline strings if required
+mail_body = """Auto email for the delivery of documents only. Please do not reply to this address."""
+# If using Windows remember the escaping \
 dir_path = "./"
 files = ["test.txt", "test2.txt"]
+file_names = ["test.txt", "test2.txt"]
+i = 0
 
 # Building email
 
@@ -30,13 +36,14 @@ mimemsg.attach(MIMEText(mail_body, 'plain'))
 for f in files:
     file_path = os.path.join(dir_path, f)
     attachment = MIMEApplication(open(file_path, "rb").read(), _subtype="txt")
-    attachment.add_header('Content-Disposition','attachment', filename=f)
+    attachment.add_header('Content-Disposition','attachment', filename=f"{file_names[i]}")
     mimemsg.attach(attachment)
+    i = i + 1
 
 # Connecting and sending email
 
 connection = smtplib.SMTP(host='smtp.office365.com', port=587)
 connection.starttls()
-connection.login(username,password)
+connection.login(username,keyring.get_password("autoemail", "email@domain.com"))
 connection.send_message(mimemsg)
 connection.quit()
